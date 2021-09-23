59dbac3e0e88c85cceca2ad5253711996fc89af3
Cristhian Veizaga Blanco <91234793+CristhianFreddyVeizagaBlanco@users.noreply.github.com>
2021-09-23 11:47:00 -0400

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.fs
@@ -0,0 +1,18 @@
+﻿// Más información acerca de F# en http://fsharp.org
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Cristhian"
+
+    let a=7
+    let b=6
+    let mutable res = 0
+    for i = 1 to a do
+       printfn "= %i" res
+       res <- res + b
+    printfn "= %i" res
+    printfn "7*6 Con sumas es = %i" res
+    Console.ReadKey() |>ignore
+    0 // devolver un código de salida entero
